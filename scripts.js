0e35ced0ac42b72b9948860b50352e9e954804a8
BLafleur <BDLAFLEUR21@gmail.com>
2024-11-28 11:37:16 -0700

    added support for mobile

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+const hamMenu = document.querySelector(".ham");
+
+const mobileMenu = document.querySelector(".mobilemenu");
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+})
